f6c1b78950bbd16ef8af542f623b4e4e17fac034
qozle <mf.qozle@gmail.com>
2021-01-28 12:22:40 -0500

    update jquery CDN

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/jquery-ui.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <style>
 #button-div {
     width: 100%;
